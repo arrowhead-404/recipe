dc42e5f82140f1c1d2375d1016e5af49f82fbec1
SDatta <sodatta@gmail.com>
2019-08-09 16:13:46 +0530

    Added JUnit and Mockito tests.

--- src/test/java/com/sd/recipe/controllers/IndexControllerTest.java
+++ src/test/java/com/sd/recipe/controllers/IndexControllerTest.java
@@ -5,6 +5,7 @@ import com.sd.recipe.repositories.RecipeRepository;
 import com.sd.recipe.service.RecipeService;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
@@ -35,12 +36,20 @@ public class IndexControllerTest {
     public void getIndexPage() {
 
         Set<Recipe> mocks=new HashSet<>();
-        mocks.add(new Recipe());
+        Recipe r1=new Recipe();
+        r1.setDescription("r1");
+        Recipe r2=new Recipe();
+        r2.setDescription("r2");
+        mocks.add(r1);
+        mocks.add(r2);
         when(recipeService.getRecipes()).thenReturn(mocks);
 
+        ArgumentCaptor<Set<Recipe>> argumentCaptor=ArgumentCaptor.forClass(Set.class);
         assertEquals(indexController.getIndexPage(model),"index");
 
         verify(recipeService,times(1)).getRecipes();
-        verify(model,times(1)).addAttribute(eq("recipes"), anySet());
+        verify(model,times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+
+        assertEquals(2,argumentCaptor.getValue().size());
     }
 }
