fe5fbfdb0ae43337815ac61c78aac800c920c1a3
SDatta <sodatta@gmail.com>
2019-08-19 18:21:39 +0530

    Handled NumberFormatException.

--- src/main/java/com/sd/recipe/controllers/RecipeController.java
+++ src/main/java/com/sd/recipe/controllers/RecipeController.java
@@ -70,4 +70,18 @@ public class RecipeController {
 
         return modelAndView;
     }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(NumberFormatException.class)
+    public ModelAndView handleNumberFormat(Exception exception){
+
+        log.error("Handling NumberFormatException");
+        log.error(exception.getMessage());
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("exception", exception);
+
+        modelAndView.setViewName("400error");
+
+        return modelAndView;
+    }
 }
